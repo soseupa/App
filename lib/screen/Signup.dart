ef41756a3b9b6fe57ce521d70da411ff0ec83a8b
parkdaxun <parkde0207@gmail.com>
2023-04-13 00:11:08 +0900

    [ADD] LoginPage, SignupPage

---
+++ lib/screen/Signup.dart
@@ -0,0 +1,194 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gaori/screen/Set_email.dart';
+import 'package:gaori/screen/Start_page.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  bool isButtonActive = false;
+  late TextEditingController controller;
+  late TextEditingController controller2;
+  late TextEditingController controller3;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TextEditingController();
+    controller.addListener(() {
+      final isButtonActive = controller.text.isNotEmpty;
+      setState(() => this.isButtonActive = isButtonActive);
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          '회원가입',
+          style: TextStyle(color: Colors.black),
+        ),
+        elevation: 0.0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back), // TODO : 아이콘 피그마에 나와 있는 걸로 바꾸기
+          color: Colors.black,
+          onPressed: () {
+            // 아이콘 버튼 실행
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => StartPage()),
+            );
+          },
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(42.0),
+            child: Center(
+              child: Image.asset('assets/image/main_logo.png',
+                  width: 109, height: 117),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(bottom: 12.0),
+                child: Text(
+                  '닉네임 설정',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(
+                      width: 320,
+                      height: 48,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: '닉네임을 입력해주세요.',
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(14.0),
+                          ),
+                          filled: true,
+                          fillColor: Color(0xffF5F5F5),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 12.0, top: 12),
+                child: Text(
+                  '비밀번호 설정',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(
+                      width: 320,
+                      height: 48,
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: '비밀번호를 입력해주세요.',
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(14.0),
+                          ),
+                          filled: true,
+                          fillColor: Color(0xffF5F5F5),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 12.0, top: 12),
+                child: Text(
+                  '비밀번호 확인',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(
+                      width: 320,
+                      height: 48,
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: '비밀번호를 입력해주세요.',
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(14.0),
+                          ),
+                          filled: true,
+                          fillColor: Color(0xffF5F5F5),
+                        ),
+                        controller: controller,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 54.0),
+              child: ElevatedButton(
+                onPressed: isButtonActive
+                    ? () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SetEmailPage()),
+                        );
+                        setState(() => isButtonActive = false);
+                      }
+                    : null,
+                child: Text(
+                  "다음",
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: ElevatedButton.styleFrom(
+                  onSurface: Color(0xffFF419C),
+                  backgroundColor: Color(0xffFF419C),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(14.0)),
+                  minimumSize: Size(320, 48),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screen/Start_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gaori/screen/Signup.dart';
+
+import 'Login.dart';
+
+class StartPage extends StatefulWidget {
+  const StartPage({Key? key}) : super(key: key);
+
+  @override
+  State<StartPage> createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset('assets/image/main_logo.png'),
+          Center(
+            child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 52),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => SignupPage()),
+                        );
+                      },
+                    child: Text("회원가입"),
+                    style: TextButton.styleFrom(
+                        primary: Colors.white, backgroundColor: Color(0xffFF419C),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(14.0)),
+                        minimumSize: Size(150, 50),
+                    ),
+
+                  ),
+                ),
+              Padding(
+                padding: const EdgeInsets.only(top: 52),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                    );
+                  },
+                  child: Text("로그인"),
+                  style: TextButton.styleFrom(
+                      primary: Color(0xffFF419C), backgroundColor: Color(0xffFFF8FB),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(14.0)),
+                      minimumSize: Size(150, 50),
+                      shadowColor: Color(0xffFF419C),
+                      //TODO : Shadow Blur 처리 넣기
+                  ),
+                ),
+              ),
+            ]),
+          )
+        ],
+      ),
+    );
+  }
+}
