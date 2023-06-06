9e430004c6b2d1b908abeb29a8011db324d042d0
soohyuneeee <soohyuneeee12@gmail.com>
2023-06-06 16:37:22 +0900

    {Add} ScheduleUser 클래스

--- lib/class/schedule.dart
+++ lib/class/schedule.dart
@@ -1,5 +1,11 @@
 class Schedule_task{
+  int id;
   String title;
-  List<String> friends;
-  Schedule_task(this.title, this.friends);
+  List<ScheduleUser> friends;
+  Schedule_task(this.id, this.title, this.friends);
+}
+class ScheduleUser{
+  int id;
+  String name;
+  ScheduleUser(this.id, this.name);
 }
