bdeb0f4e6b9a43fb0dc177794058effcc6ced1dc
soohyuneeee <soohyuneeee12@gmail.com>
2023-06-09 10:16:07 +0900

    {Feat} map 웹뷰 화면

--- lib/screen/map.dart
+++ lib/screen/map.dart
@@ -1,20 +1,60 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class MapTest extends StatelessWidget {
-  String url = ""; // 띄울 웹 페이지의 주소
-  Set<JavascriptChannel>? channel; // 아래 설명 참조
-  WebViewController? controller; // 아래 설명 참조
+import 'Login.dart';
+
+class MapPage extends StatefulWidget {
+  final int id;
+  final String title;
+
+  MapPage({required this.id, required this.title});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  Token? inputData = InputData.inputData;
+  late WebViewController controller;
+
+  @override
+  void initState() {
+    String token = inputData?.token ?? "";
+    print('token : $token');
+    print('ID : ${widget.id}');
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(),
+      )
+      ..loadRequest(Uri.parse(
+          'http://34.64.108.196/map?id=${widget.id}&token=$token'));
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return WebView(
-        initialUrl: url,
-        onWebViewCreated: (controller) {
-          this.controller = controller;
-        },
-        javascriptChannels: channel,
-        javascriptMode: JavascriptMode.unrestricted // 자바스크립트 허용
-        );
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        toolbarHeight: 60,
+        iconTheme: IconThemeData(
+          color: Colors.black,
+        ),
+        centerTitle: true,
+        title: Text(
+          '${widget.title}',
+          style: TextStyle(
+              color: Colors.black,
+              fontSize: 19,
+              fontFamily: 'NotoSansKR',
+              fontWeight: FontWeight.w600
+          ),
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: WebViewWidget(controller: controller),
+    );
   }
 }
