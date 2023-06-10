2163472761778ac68fa3f686f42cbca44c4e6214
soohyuneeee <soohyuneeee12@gmail.com>
2023-06-10 21:07:32 +0900

    {Fix} degub 위에 뜨느거 제거

--- lib/main.dart
+++ lib/main.dart
@@ -3,27 +3,25 @@ import 'package:gaori/screen/Start_page.dart';
 import 'package:gaori/screen/calendar.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
-Future main() async {
-  //Splash 화면 나타나게 하는 코드!!
-  WidgetsFlutterBinding.ensureInitialized(); // 초기화 보장
-  await Future.delayed(const Duration(seconds: 10)); // 3초 지연
-  initializeDateFormatting().then((_) => runApp(MaterialApp(
-        home: MyPage(),
-      )));
-
+void main() {
+  runApp(MyPage());
 }
-
+// Future main() async {
+//   //Splash 화면 나타나게 하는 코드!!
+//   WidgetsFlutterBinding.ensureInitialized(); // 초기화 보장
+//   await Future.delayed(const Duration(seconds: 10)); // 3초 지연
+//   initializeDateFormatting().then((_) => runApp(MaterialApp(
+//     home: MyPage(),
+//   )));
 class MyPage extends StatelessWidget {
   const MyPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
           title: '가오리',
           debugShowCheckedModeBanner: false,
           home: StartPage()
-
     );
   }
 }
